55f2dd5d1b83009831dc078937b5e39730fac51f
Shoaib Khan <shoaib7878687@gmail.com>
2026-04-08 13:00:56 +0500

    updated changes

--- React Todo/src/TodoList.jsx
+++ React Todo/src/TodoList.jsx
@@ -8,13 +8,53 @@ export default function Todolist(){
     let [newtodo , setNewtodo] = useState ([]);
 
     let addNewtasks = () =>{
-      setTodos([...todos, {tasks : newtodo, id: uuidv4()}])
+      setTodos((pretodo)=>{
+        return [...pretodo, {tasks : newtodo, id: uuidv4()}]
+      })
       setNewtodo("")
+    }
+
+    let deletebtn = (id) =>{
+        setTodos(todos.filter((todo) => todo.id !=id))
     }
 
     let Newtodolist = (event) => {
         setNewtodo(event.target.value)
     }
+
+    let Uppercase = () =>{
+        setTodos (
+            todos.map((todo) =>{
+                return {
+                    ...todo ,
+                    tasks : todo.tasks.toUpperCase(), 
+                }
+            } )
+        )
+    }
+    
+    let Lowercase = () =>{
+        setTodos (
+            todos.map ((todo) => {
+                return {
+                    ...todo,
+                    tasks : todo.tasks.toLowerCase(),
+                }
+            })
+        )
+    }
+
+let Editbtn = (id) => {
+  let newText = prompt("Enter new value");
+
+  if (!newText) return;
+
+  setTodos(prevTodos =>
+    prevTodos.map(todo =>
+      todo.id === id ? { ...todo, tasks: newText } : todo
+    )
+  );
+};
     return (
         <div className="todo-container">
             <h3>Todo List</h3>
@@ -26,10 +66,20 @@ export default function Todolist(){
             <ul>
                 {
                     todos.map ((todo) => (
-                        <li key={todo.id}>{todo.tasks}</li>
+                        <li key={todo.id}>
+                            {todo.tasks}
+                            <button onClick={() => deletebtn(todo.id)} className="btn1">Delete</button>
+                            <button onClick={() => Editbtn(todo.id)} className="btn">Edit</button>
+                        </li>
                     ))
                 }
             </ul>
+
+            <div>
+                <button onClick={Uppercase} className="btn">UpperCase</button>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <button onClick={Lowercase} className="btn">LowerCase</button>
+            </div>
         </div>
     )
 }
